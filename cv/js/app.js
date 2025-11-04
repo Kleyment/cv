e328e5b97e0c347c4962f21d78dda20743562e0f
BELLANGER Clement <clement.bellanger@virgin-mojito.fr>
2025-11-04 15:06:12 +0100

    Add 2 different Pfp 'good' blue and 'bad' red, and a preview function with CSS to display special projets

--- cv/js/app.js
+++ cv/js/app.js
@@ -1,125 +1,3 @@
-$(document).ready(function(){
-
-    $('#profile__ripple').ripples({
-        resolution: 512,
-        dropRadius:10
-    });
-
-    const bars = document.querySelectorAll('.progress__bar');
-
-    bars.forEach(function(bar){
-        let percentage = bar.dataset.percent;
-        let tooltip = bar.children[0];
-        //tooltip.innerText = percentage + '%';
-        bar.style.width = percentage + '%';
-    })
-
-    // Counter
-
-    const counters = document.querySelectorAll('.counter');
-
-    function runCounter() {
-        counters.forEach(counter => {
-            counter.innerText = 0;
-            let target = +counter.dataset.count;
-            let step = target / 100;
-            
-            let countIt = function() {
-                let displayedCount = +counter.innerText;
-                if(displayedCount < target) {
-                    counter.innerText = Math.ceil(displayedCount + step);
-                    setTimeout(countIt, 1);
-                } else {
-                    counter.innerText = target;
-                }
-            }
-            countIt();
-
-        })
-
-    }
-
-  
-
-    let counterSection = document.querySelector('.counter__section');
-
-    let options = {
-        rootMargin : '0px 0px -200px 0px'
-    }
-    let done = 0; 
-
-    const sectionObserver = new IntersectionObserver(function(entries){
-
-        if(entries[0].isIntersecting && done !== 1) {
-            done = 1;
-            runCounter();
-        }
-    }, options)
-
-    sectionObserver.observe(counterSection);
-
-
-
-    // image filter 
-
-    var $wrapper = $('.portfolio__wrapper');
-    // Initialize isotope 
-    $wrapper.isotope({
-        filter : '*',
-        layoutMode : 'masonry',
-        animationOptions : {
-            duration: 750,
-            easing: 'linear'
-        }
-    });
-
-    let links = document.querySelectorAll('.tabs a');
-
-    links.forEach(link => {
-
-        let selector = link.dataset.filter;
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-
-            $wrapper.isotope({
-                filter : selector,
-                layoutMode : 'masonry',
-                animationOptions : {
-                    duration: 750,
-                    easing: 'linear'
-                }
-            });
-
-            links.forEach(link =>{
-                link.classList.remove('active');
-            })
-
-            e.target.classList.add('active');
-
-        });
-    })
-
-
-    // Magnify pop up 
-console.log($('.magnify').magnificPopup)
-    $('.magnify').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled : true
-        },
-        zoom : {
-            enable: true
-        }
-    });
-
-    // Slider
-
-    $('.slider').slick({
-        arrows: false,
-        autoplay: true
-    });
-});
-
 function displayLanguageInfos(langNum,mouseOver) {
     if (langNum==1) {
         if (mouseOver) {
@@ -136,4 +14,53 @@ function displayLanguageInfos(langNum,mouseOver) {
             $('#lang2').css("right","-15px");
         }
     }
+}
+
+function changeUserPhoto(isUserbad) {
+    if (isUserbad == true) {
+        document.getElementById("userPhoto").src="./cv/images/baduser.jpg";
+    } else {
+        document.getElementById("userPhoto").src="./cv/images/gooduser.jpg";
+    }
+}
+
+function previewImage(imageName, display) {
+    if (display) {
+        document.getElementById(imageName).style['display']="";
+    } else {
+        document.getElementById(imageName).style['display']="none";
+    }
+    //Put the image on the good tag
+    //var imgNode=document.createElement("img");
+    //imgNode.src="./cv/images/"+imageName+".png";
+    
+
+    //var bodyNode=document.getElementsByTagName("body")[0];
+    //bodyNode.appendChild(imgNode);
+
+
+    /**
+     * CSS beforeSecuJS
+     * z-index: 0;
+     * 
+     */
+
+    /**
+     * CSS (image animated) secuJS img
+     * 
+     * position: absolute;  
+     * width: 5%;
+     * z-index: -1; -> 1;
+     * filter: blur(5px); -> blur(0px)
+     * rotate: 90deg; -> 0;
+     * display: none -> display: ''
+     * 
+     */
+
+    /**
+     * CSS container (body)
+     * 
+     * position: fixed;
+     * z-index: 0;
+     */
 }
